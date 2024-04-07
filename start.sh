57696b459d330b71fae4837f54b50fcba7efe1cf
cristianoliveira <contato@cristianoliveira.com.br>
2024-04-07 09:23:18 +0200

    refact: scripts with meaningful names

--- start.sh
+++ start.sh
@@ -15,16 +15,16 @@ function close_childs_on {
 
 trap close_childs_on EXIT
 
-# Load files from proxy.json
+# Load files from proxy.json and store the pids in proxy.pid
+rm -f $PWD/proxy.pid
 for i in $(jq -r '.providers[].name' proxy.json); do
   echo "Starting proxy for $i"
   ## get the file and port from { "name": "file", "port": "port" }
   file=$(jq -r --arg i $i '.providers[] | select(.name == $i) | .openapiFile' proxy.json)
   port=$(jq -r --arg i $i '.providers[] | select(.name == $i) | .port' proxy.json)
 
-  echo "npm run mock -d $file -p $port"
-  bash -c "npm run mock -- -d $file -p $port & echo \$! >> $PWD/proxy.pid"
+  echo "npm run prism:mock -d $file -p $port"
+  bash -c "npm run prism:mock -- -d $file -p $port & echo \$! >> $PWD/proxy.pid"
 done
 
-npm run start
-
+npm run proxy:serve
