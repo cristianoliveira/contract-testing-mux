3bd7fca233cb6371efe46e732441052053af469a
cristianoliveira <contato@cristianoliveira.com.br>
2024-04-10 12:24:44 +0200

    feat: adds infra initial setup

--- infra/main.tf
+++ infra/main.tf
@@ -7,14 +7,8 @@ resource "aws_instance" "app" {
   vpc_security_group_ids = [module.webapp_sg.security_group_id]
   associate_public_ip_address = true
 
-  iam_instance_profile = aws_iam_instance_profile.ecr_profile.name
-
   user_data = templatefile("./tpls/setup.tpl.sh", {
     description   = "foo bar"
-    aws_ecr_url   = var.aws_ecr_url
-    aws_region    = var.aws_region
-    app_image_tag = var.app_image_tag
-    app_db_url    = var.app_db_url
     port          = "8080"
   })
 
